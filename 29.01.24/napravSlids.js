cef50181afb70d1e5ef25238c48c77e0ad3e8739
collegesp3 <151716191+collegesp3@users.noreply.github.com>
2024-02-08 12:15:52 +0300

    попытки подобрать другой подход к слайдеру

---
+++ 29.01.24/napravSlids.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('myButton');
+let i = 0;
+button.addEventListener('click', function () {
+    let elm = document.body.appendChild(document.createElement('div'));
+
+    
+    //let r = "<p>Привет!</p>";
+    //q.innerHTML = r;
+    
+    i ++;  
+    console.log(i);
+    
+    //var newDiv = document.createElement("div");
+    //newDiv.innerHTML = "<h1>Привет!</h1>";
+  }); 
