a034c12cd09d6c44b5b146e7ee930f5ce322d57e
collegesp3 <151716191+collegesp3@users.noreply.github.com>
2024-02-09 12:20:04 +0300

    трансформация в js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function onScroll() {
   var top = window.pageYOffset;
   if (scroll > top) {
     document.getElementById("header_menu").style.display = "";
+    //document.getElementById("header_menu").style.transition = "all 1,5s";
   } else if (scroll < top) {
     document.getElementById("header_menu").style.display = "none";
+    //document.getElementById("header_menu").style.transition = "all 1,5s";
   }
   scroll = top;
 }
